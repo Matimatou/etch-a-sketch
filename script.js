405641aa982a03804c73e244922bf9165c34ade2
Mathias <Inutile0@protonmail.com>
2023-06-18 01:06:29 +0200

    feat: Set up hover effect - add an event listener to each cases - done by iterating trough nodelistedocument.querySelectorAll('.case')

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-const GRIDWIDTH = 14;
+let GRIDWIDTH = 16;
 const body = document.querySelector('body');
 
 const grid = document.querySelector('.grid');
 /*Compute the correct size of Width in order that the grid looks rectangular*/
 function sizeGrid() {
-    grid.offsetWidth = '1500px';
+    grid.offsetWidth = 'fit-content';
 }
 function computeWidth() {
     return [GRIDWIDTH, GRIDWIDTH];
 }
 function createGrid() {
     for (let i = 0; i < computeWidth()[0]; i++) {
-        const line = document.createElement('div');
+        const column = document.createElement('div');
+        column.classList.add("column")
         for (let j = 0; j < computeWidth()[1]; j++) {
             const div = document.createElement('div');
             div.classList.add('case');
-            div.textContent = `${i * GRIDWIDTH + j}`;
-            line.appendChild(div);
-            console.log(line);
+            //div.textContent = `${i * GRIDWIDTH + j}`;
+            column.appendChild(div);
         }
-        grid.appendChild(line);
+        grid.appendChild(column);
     }
 }
-createGrid(); // CETTE LIGNE JE L'AVAIS OUBLIÉ
-// TO-DO : divise the cases in lines so that they can be aligned easly  with flex display
+
+
+createGrid(); // CETTE LIGNE JE L'AVAIS OUBLIÉE
+const gridArray = document.querySelectorAll('.case');
+
+function createHoverEffect() {
+    function addHoverEffectToCase(element, color) {
+        return () => { element.style.backgroundColor = color };
+    }
+    // Loop on the cases
+    gridArray.forEach(element => {
+        console.log(typeof (element));
+        element.addEventListener('mouseover', addHoverEffectToCase(element, 'black'));
+    });
+}
+createHoverEffect();
+
