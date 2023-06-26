aa2f7ff76dd7351d14c6a30d2842f9d631531b09
APARICIO Mathias <maparicio001@enseirb.fr>
2023-06-26 18:53:38 +0200

    feat: Add darking effect - On first mouseover on a div, div get random color - And on the next 10 mouseover div get 10% darker until completly dark

--- script.js
+++ script.js
@@ -24,11 +24,38 @@ function createGrid(gridwidth) {
     return document.querySelectorAll('.case')
 }
 function createHoverEffect(gridArray) {
+    function objectToColor(object) {
+        return `rgb(${object.red},${object.green},${object.blue})`;
+    }
+    /* What I want to do : have the color goes from random to black in n times
+    Idea : Write a function that when called the ith time return i/n time way black
+    and when i is greater than n does nothing or simple is not call*/
     function addHoverEffectToCase(element, color) {
+        let randomColor;
         if (color !== 'random')
             return () => { element.style.backgroundColor = color };
-        else
-            return () => { element.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})` };
+        else {
+            function darkenColor(color, step) {
+                return {
+                    red: color.red * (1 - step * 0.1),
+                    green: color.green * (1 - step * 0.1),
+                    blue: color.blue * (1 - step * 0.1)
+                }
+            }
+            let count = 0;
+            function darkenOnMouseOver() {
+                if (count == 0) {
+                    randomColor = { red: Math.floor(Math.random() * 255), green: Math.floor(Math.random() * 255), blue: Math.floor(Math.random() * 255) }
+                    element.style.backgroundColor = objectToColor(randomColor);
+                }
+                else if (count <= 10) {
+                    const darkedColor = darkenColor(randomColor, count);
+                    element.style.backgroundColor = objectToColor(darkedColor);
+                }
+                count++;
+            }
+            return darkenOnMouseOver;
+        }
     }
     // Loop on the cases
     gridArray.forEach(element => {
